72ce92532a7481f16cf4c1efc95a7de7745c8b9f
marek3jan <107478816+marek3jan@users.noreply.github.com>
2024-02-11 23:55:14 +0100

    Update Articles_scraper.py

--- Articles_scraper.py
+++ Articles_scraper.py
@@ -6,36 +6,68 @@ import sys
 
 arguments = list(sys.argv)
 
-def give_me_articles():
-    core_html = BeautifulSoup(get(webpage).text, features="html.parser")
+def define_source():
+    core_source = BeautifulSoup(get(web_page).text, features="html.parser")
 
-    if not core_html.find('div', {'class':  'docsum-content'}):
-        return print("Your webpage is wrong".upper())
+    if core_source.find('div', {'class': 'docsum-content'}):
+        source = BeautifulSoup(get(web_page).text, features="html.parser")
 
     else:
-        print(f'Downloading data from the chosen webpage: {webpage}')
-        pages = (core_html.find('label', {'class': 'of-total-pages'})).text.strip('of ')
-        pages = int(pages)
-        content = []
-        content.append(webpage)
+        source = 'none'
 
-        articles = []
-        references = []
-        journals = []
-        years = []
-        doi = []
+    return source
 
-        for i in range(2, pages+1):
-            if pages >= 2:
-                page = webpage + f"&page={i}"
-                content.append(page)
-            else:
+def select_pages():
+    source = define_source()
+    if source != 'none':
+        pages = int((source.find('label', {'class': 'of-total-pages'})).text.strip('of '))
+
+    else:
+        pages = 'none'
+
+    return pages
+
+def create_content_archive():
+    content_archive: list = []
+    pages = select_pages()
+    if pages != 'none':
+        content_archive.append(web_page)
+
+        for i in range(2, pages + 1):
+            if pages < 2:
                 continue
+            else:
+                page = web_page + f"&page={i}"
+                content_archive.append(page)
+
+    else:
+        content_archive = 'none'
+
+    return content_archive
+
+def create_list_of_information():
+    content = create_content_archive()
+
+    if content != 'none':
+        list_of_information: list = []
+
+        webdoi = []
+        doi = []
+        years = []
+        journals = []
+        articles = []
 
         for i in range(len(content)):
             html = BeautifulSoup(get(content[i]).text, features="html.parser")
-            information = html.find_all('div', {'class':  'docsum-content'})
+            information = html.find_all('div', {'class': 'docsum-content'})
             links = html.find_all('a', {'class': 'docsum-title'})
+
+            for direction in links:
+                title = direction.text.encode('ascii', 'ignore').decode()
+                title = title.strip('\n              \n')
+                title = title.strip('\n')
+                title = title.strip('              ')
+                articles.append(title)
 
             for info in information:
                 if not info.find('span', {'class': 'docsum-journal-citation full-journal-citation'}):
@@ -52,9 +84,11 @@ def give_me_articles():
                         idoi = idoi[0][0:-1]
                     else:
                         idoi = idoi[0]
-                    idoi = f'https://doi.org/{idoi}'
+
+                    linkdoi = f'https://doi.org/{idoi}'
 
                 doi.append(idoi)
+                webdoi.append(linkdoi)
 
                 if info.find('span', {'class': 'docsum-journal-citation short-journal-citation'}):
                     cit = info.find('span', {'class': 'docsum-journal-citation short-journal-citation'}).text
@@ -68,23 +102,33 @@ def give_me_articles():
                 years.append(year)
                 journals.append(journal)
 
-            for direction in links:
-                link = "/".join(content[i].split("/")[:-1]) + "/" + direction['href']
-                references.append(link)
+        list_of_information.append(webdoi)
+        list_of_information.append(doi)
+        list_of_information.append(years)
+        list_of_information.append(journals)
+        list_of_information.append(articles)
 
-                title = direction.text.encode('ascii', 'ignore').decode()
-                articles.append(title)
+    else:
+        list_of_information = 'none'
 
-        for i in range(len(articles)):
-            dictionary = {}
-            articles[i] = articles[i].strip('\n              \n')
-            articles[i] = articles[i].strip('\n')
-            articles[i] = articles[i].strip('              ')
-            dictionary['DOI_WEBPAGE'] = doi[i]
-            dictionary['PUBMED_WEBPAGE'] = references[i]
-            dictionary['YEAR'] = years[i]
-            dictionary['JOURNAL'] = journals[i]
-            dictionary['TITLE'] = articles[i]
+    return list_of_information
+
+
+def create_dictionary_and_write_to_csv():
+    my_list = create_list_of_information()
+
+    if my_list == 'none':
+        Termination = True
+        return Termination
+
+    else:
+        for i in range(len(my_list[4])):
+            dictionary: dict = {}
+            dictionary['Web Source'] = my_list[0][i]
+            dictionary['DOI'] = my_list[1][i]
+            dictionary['Year'] = my_list[2][i]
+            dictionary['Scientific Journal'] = my_list[3][i]
+            dictionary['Article Title'] = my_list[4][i]
 
             mode = "w" if f'{file_name}' not in os.listdir() else "a"
             with open(f'{file_name}', mode, newline='') as file:
@@ -96,9 +140,6 @@ def give_me_articles():
                     writer.writeheader()
                 w.writerow(row)
 
-        return print('Your csv file has been generated'.upper())
-
-
 
 if __name__ == "__main__":
     if len(arguments) < 2:
@@ -108,6 +149,9 @@ if __name__ == "__main__":
     else:
         url = input("Webpage of interest: ".upper())
         arguments.insert(1, url)
-        webpage = str(arguments[1])
+        web_page = str(arguments[1])
         file_name = str(arguments[2]) + '.csv'
-        give_me_articles()
+
+        create_dictionary_and_write_to_csv()
+
+        print("Your csv file should be ready, otherwise you selected wrong website".upper())
